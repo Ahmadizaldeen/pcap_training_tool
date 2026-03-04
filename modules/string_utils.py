0a2b8945595a9fd717467db39afdac7e866ea6cf
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-03-04 20:24:03 +0100

    install Package faker . Zufällige Text , words, litter in string_quiz , und export zu run_string

--- modules/string_utils.py
+++ modules/string_utils.py
@@ -1,3 +1,6 @@
+from faker import Faker
+
+fake = Faker()
 
 def find_substring(s, sub):
     return s.find(sub)
@@ -14,3 +17,18 @@ def split_string(s, sep=None):
     return s.split(sep)
 
 
+def string_quiz ():
+    text = fake.text(70)
+    words = text.split()
+    litter = list(words[0] + words[1])
+    
+
+    return text,words,litter
+
+
+def run_string():
+    text, words, litter = string_quiz()
+    print(text)
+
+if __name__ == "__main__":
+    run_string()
