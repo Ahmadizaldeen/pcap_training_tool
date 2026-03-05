db467dd796ee490f4fe421e698db9811cebb1422
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-03-05 23:14:49 +0100

    func_dict, funktionen sortern

--- modules/string_utils.py
+++ modules/string_utils.py
@@ -25,7 +25,7 @@ class StringMethods:
     def len_str(sting):
         word_lengths= {w:len(w)for w in sting}
         print("Länge:",word_lengths)
-string_funktionen = [["split()","String → Liste"],
+string_funktionen = [["split","String → Liste"],
                      ["strip","Entfernt Leerzeichen (oder andere Zeichen, wenn angegeben)"],
                      ["Palindrom","[::-1] Schreibe den Text rückwärts"],
                      ["replace","ersetzt Teilstring" ],
@@ -45,20 +45,32 @@ string_funktionen = [["split()","String → Liste"],
                      ["sort", "❌ AttributeError bei str"]]
 
 
-
+func_dict = {"general_method" : ["index","find","rfind"],
+             "text_method" : ["split","strip","replace", "title","capitalize" ],
+             "word_method" :["join","upper","lower"],
+             "liter_method" : ["count","startswith","endswith","sorted"],
+             "other_method":["Palindrom","casefold","sort"]}
 
 def string_quiz ():
     str_func = string_funktionen.copy()
     shuffle(str_func)
     get_func = choice(str_func)
     func_name, desc = get_func
-    #print(get_func)
-
-
-
     text = fake.text(70)
     words = [w.strip(".,'") for w in text.split()]
     litter = list(words[0] + words[1])
+
+    if func_name in func_dict.get("general_method"):
+        print("general_method")
+    elif func_name in func_dict.get("text_method"):
+        print("text_method")
+    elif func_name in func_dict.get("word_method"):
+        print("word_method")
+    elif func_name in func_dict.get("liter_method"):
+        print("liter_method")
+    elif func_name in func_dict.get("other_method"):
+        print("other_method")
+
     question_text = f"what is the output from : {func_name}({words[2]})"
     print(question_text)
     print(list(map(lambda word: word.title(), words[0:3])))
@@ -78,3 +90,9 @@ if __name__ == "__main__":
     run_string()
     ue ="ß".casefold()
     print(ue)
+    try:
+        raise Exception
+    except Exception:
+        print("b")
+    except :
+        print("a")
