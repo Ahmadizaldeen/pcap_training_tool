b3a2c22ff2fe9e14d7137786c3dfc0958ce03bf6
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-03-12 00:54:25 +0100

    type_color_decorator, debug decorator moved to decorators_dome

--- modules/decorators_dome.py
+++ modules/decorators_dome.py
@@ -74,8 +74,74 @@ def run_demo():
     MyClass.class_method()
 
 
-# =============================
-# Ausführung direkt über Modul
-# =============================
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[0;31m'
+CYAN = '\033[96m'
+MAGENTA = '\033[95m'
+ENDC = '\033[0m'
+
+def colorize(value):
+
+    if isinstance(value, int):
+        color = BLUE
+    elif isinstance(value, float):
+        color = MAGENTA
+    elif isinstance(value, str):
+        color = GREEN
+    elif isinstance(value, list):
+        color = CYAN
+    elif isinstance(value, dict):
+        color = YELLOW
+    elif isinstance(value, tuple):
+        color = RED
+    else:
+        color = ENDC
+
+    return f"{color}{value}{ENDC}"
+
+def type_color_decorator(func):
+    print(f"{BLUE}int {GREEN}str {YELLOW}dict {RED}list {CYAN}list {MAGENTA}float{ENDC}")
+
+    def wrapper(*args, **kwargs):
+
+        result = func(*args, **kwargs)
+
+        return colorize(result)
+
+    return wrapper
+
+def debug(func):
+
+    def wrapper(*args, **kwargs):
+
+        print(f"[DEBUG] Function: {func.__name__}")
+        print(f"[DEBUG] args: {args}")
+        print(f"[DEBUG] kwargs: {kwargs}")
+
+        result = func(*args, **kwargs)
+
+        print(f"[DEBUG] result: {result}")
+
+        return result
+
+    return wrapper
+
+def colored_class(default_color):
+    RESET = '\033[0m'
+    def decorator(cls):
+        original_str = cls.__str__ if hasattr(cls, "__str__") else lambda self: f"{self.__class__.__name__}"
+
+        def new_str(self):
+            return f"{default_color}{original_str(self)}{RESET}"
+
+        cls.__str__ = new_str
+        return cls
+
+    return decorator
+
 if __name__ == "__main__":
+
+    print(f"{BLUE}int {GREEN}str {YELLOW}dict {RED}list {CYAN}list {MAGENTA}float{ENDC}")
     run_demo()
