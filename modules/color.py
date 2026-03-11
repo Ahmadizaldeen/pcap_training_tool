4cec7082f642793177d7359bc62c191906b5f4a7
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-03-12 00:55:34 +0100

    delete color.py

--- modules/color.py
+++ modules/color.py
@@ -1,13 +1,13 @@
-
-def colored_class(default_color):
-    RESET = '\033[0m'
-    def decorator(cls):
-        original_str = cls.__str__ if hasattr(cls, "__str__") else lambda self: f"{self.__class__.__name__}"
-
-        def new_str(self):
-            return f"{default_color}{original_str(self)}{RESET}"
-
-        cls.__str__ = new_str
-        return cls
-
-    return decorator
+#
+# def colored_class(default_color):
+#     RESET = '\033[0m'
+#     def decorator(cls):
+#         original_str = cls.__str__ if hasattr(cls, "__str__") else lambda self: f"{self.__class__.__name__}"
+#
+#         def new_str(self):
+#             return f"{default_color}{original_str(self)}{RESET}"
+#
+#         cls.__str__ = new_str
+#         return cls
+#
+#     return decorator
