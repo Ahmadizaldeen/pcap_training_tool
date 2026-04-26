07a32c77a2ef4463042886813c3f2abfb61f4696
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-26 03:13:03 +0200

    Refactor classes and improve code structure in oop_examples.py

--- modules/oop_examples.py
+++ modules/oop_examples.py
@@ -1,30 +1,32 @@
-
-class Car:
-    def __init__(self, brand, wheels, model):
-        self.brand = brand
-        self.wheels = wheels
-        self.model = model
-    def description(self):
-        return f'{self.brand} {self.model} mit {self.wheels} Rädern'
+from modules.decorators_dome import type_color_decorator, debug
 
 
-#mehre objekt
-#objekt speicheren
-#vererbung einbauen
+# ---------------------------------------------------------------------------
+# Vererbung (Inheritance)
+# ---------------------------------------------------------------------------
 
-class Book:#Vererbung (inheritance) beschreibt eine Spezialisierung.
+class Book:  # Vererbung: Spezialisierung
     pass
 
-class EBook(Book):#Ein EBook ist ein Book.
+class EBook(Book):  # EBook ist ein Book
     pass
+
+
+# ---------------------------------------------------------------------------
+# Komposition (Composition)
+# ---------------------------------------------------------------------------
 
 class Author:
     pass
 
-class Book:#Komposition (composition) beschreibt Aufbau aus Teilen.
+class BookWithAuthor:  # Komposition: Book hat einen Author
     def __init__(self):
-        self.author = Author() #Ein Book hat einen Author.
+        self.author = Author()
 
+
+# ---------------------------------------------------------------------------
+# Strategy Pattern
+# ---------------------------------------------------------------------------
 
 class StorageStrategy:
     def save(self, book):
@@ -33,125 +35,86 @@ class StorageStrategy:
 class FileStorage(StorageStrategy):
     ...
 
-class BookRepository:# Repository hat eine Speicherstrategie → austauschbar ohne Umbau.
+class BookRepository:  # Repository hat eine Speicherstrategie - austauschbar ohne Umbau
     def __init__(self, storage):
         self.storage = storage
-from modules.color_console import type_color_decorator, debug
-from modules.color_in_consol import WHITE
 
+
+# ---------------------------------------------------------------------------
+# Car - einfache Klasse
+# ---------------------------------------------------------------------------
 
 class Car:
     def __init__(self, brand, wheels, model):
         self.brand = brand
         self.wheels = wheels
         self.model = model
+
     def description(self):
-        return f'{self.brand} {self.model} mit {self.wheels} Rädern'
+        return f'{self.brand} {self.model} mit {self.wheels} Raedern'
 
 
-#mehre objekt
-#objekt speicheren
-#vererbung einbauen
+# ---------------------------------------------------------------------------
+# Product - Klassenmethode, Decorator, Static
+# ---------------------------------------------------------------------------
 
-class Book:#Vererbung (inheritance) beschreibt eine Spezialisierung.
-    pass
-
-class EBook(Book):#Ein EBook ist ein Book.
-    pass
-
-class Author:
-    pass
-
-class Book:#Komposition (composition) beschreibt Aufbau aus Teilen.
-    def __init__(self):
-        self.author = Author() #Ein Book hat einen Author.
-
-
-class StorageStrategy:
-    def save(self, book):
-        pass
-
-class FileStorage(StorageStrategy):
-    ...
-
-class BookRepository:# Repository hat eine Speicherstrategie → austauschbar ohne Umbau.
-    def __init__(self, storage):
-        self.storage = storage
-BLUE = '\033[94m'
-GREEN = '\033[92m'
-YELLOW = '\033[93m'
-RED = '\033[0;31m'
-ENDC = '\033[0m'
-
-# from color import colored_class
-# @colored_class
 class Product:
     count = 0
-    def __init__(self, name, price):
 
+    def __init__(self, name, price):
         self._name = name
         self._price = price
         self._other = []
-        # Product.count += 1
-        # self.id = Product.count
         self.id = Product.get_no()
 
     @type_color_decorator
     def get_name(self):
         return self._name
 
-    #@debug
     @type_color_decorator
     def get_price(self):
         return self._price
 
-
     def set_price(self, price):
         self._price = price
-
-    #@type_color_decorator
 
     @classmethod
     def get_no(cls):
         cls.count += 1
         return cls.count
 
+
 class ProductHelper:
 
-    @staticmethod # @staticmethod verhält sich wie eine ganz normale Funktion, kein self , cls. und kann direkt über die Klasse aufgerufen werden
+    @staticmethod
     def add(x, y):
         """Addiert zwei Zahlen"""
         return x + y
 
     @staticmethod
     def greet(name):
-        """Gibt eine Begrüßung zurück"""
+        """Gibt eine Begruessung zurueck"""
         return f"Hello, {name}!"
 
 
-auto = Product("Auto", 100)
+# ---------------------------------------------------------------------------
 
-print(auto.get_name())
-print(auto.get_price())
+if __name__ == "__main__":
+    auto = Product("Auto", 100)
+    print(auto.get_name())
+    print(auto.get_price())
 
-auto.set_price(200)
+    auto.set_price(200)
+    print(auto.get_price())
 
-print(auto.get_price())
+    pc = Product("LapTop", 300)
+    print(pc.get_name())
+    print(Product.get_price(pc))
+    print(Product.get_no())
+    print(ProductHelper.add(0, 2))
 
-print(auto.get_price())
+    tool = Product("Tool", 100)
+    print(Product.get_no())
 
-
-pc = Product("LapTop", 300)
-
-print(pc.get_name())
-print(Product.get_price(pc))
-print(Product.get_no())
-print(ProductHelper.add(0,2))
-
-tool = Product("Tool", 100)
-tool3 = Product("Tool", 100)
-tool2 = Product("Tool", 100)
-print(Product.get_no()) #+1
-tool4 = Product("Tool", 100)
-tool5 = Product("Tool", 100)
-print(pc.get_no())
+    car = Car("Audi", 4, "A4")
+    print(car.description())
